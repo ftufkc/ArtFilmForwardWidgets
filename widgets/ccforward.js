0bc357b4535c58236e6a04e7a6d1150e9118dc83
ftufkc <1320067320xh@gmail.com>
2025-06-28 00:56:33 +0800

    forward官方tmdb接入

--- widgets/ccforward.js
+++ widgets/ccforward.js
@@ -9,7 +9,7 @@
 WidgetMetadata = {
     id: "criterion_collection",
     title: "Criterion Collection",
-    version: "1.1.0",
+    version: "1.2.0",
     requiredVersion: "0.0.1",
     description: "完整的标准收藏(CC)电影列表",
     author: "ddueh",
