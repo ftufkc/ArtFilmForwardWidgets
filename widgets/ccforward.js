ba7ddea759a9c7cea27a5ab2b9203b6349a70abc
ftufkc <1320067320xh@gmail.com>
2025-06-28 00:41:09 +0800

    forward官方tmdb接入

--- widgets/ccforward.js
+++ widgets/ccforward.js
@@ -116,7 +116,7 @@ async function enrichItemsWithTmdb(items) {
  */
 function mapToForwardDataModel(enrichedItems) {
     // 这里会过滤tmdb为null的部分，单可能会导致单个page数据小于20，我觉得是可以接受的
-    return enrichedItems.filter(item => item).map(item => {
+    return enrichedItems.filter(item => item.id).map(item => {
         console.log(item);
         return {
             id: item.id,
