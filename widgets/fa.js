5c427d335fd0ba4a61f635bb9d81a43fd53661f8
ftufkc <1320067320xh@gmail.com>
2025-12-13 18:42:22 +0800

    新增电影资料馆片单追踪

--- widgets/fa.js
+++ widgets/fa.js
@@ -1,7 +1,7 @@
 WidgetMetadata = {
     id: "fa",
     title: "Film Archive",
-    version: "1.2.3",
+    version: "1.2.4",
     requiredVersion: "0.0.1",
     description: "中国电影资料馆与上海电影资料馆是中国大陆重要的电影档案与放映机构，本合集用于追踪其策划的展映片单",
     author: "ddueh",
@@ -9,14 +9,14 @@ WidgetMetadata = {
         id: "fa",
         title: "Film Archive",
         functionName: "getCollections",
-        cacheDuration: 86400,
+        cacheDuration: 3600,
         params: []
     }]
 };
 
 async function getCollections(params = {}) {
     const response = await Widget.http.get("https://forward.lilychou.cn/api/data");
-    let data = response.data;
+    let data = response.data.data;
     return getRandomArray(data);
 }
 
